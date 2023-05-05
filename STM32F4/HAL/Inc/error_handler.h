7aa4c3e6ad562c3be3c21d383ff23edf304feec7
scottc11 <scott.m.campbell11@gmail.com>
2023-05-04 23:43:19 -0400

    removed hard copy of stm32f4 folder

--- STM32F4/HAL/Inc/error_handler.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "common.h"
-#include "gpio_api.h"
-
-HAL_StatusTypeDef error_handler(HAL_StatusTypeDef error);
--- STM32F4/HAL/Inc/tim_api.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-uint32_t tim_get_APBx_freq(TIM_HandleTypeDef *htim);
-
-uint32_t tim_get_overflow_freq(TIM_HandleTypeDef *htim);
-
-void tim_set_overflow_freq(TIM_HandleTypeDef *htim, uint32_t target_freq);
